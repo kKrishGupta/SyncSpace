7d541fd6951ff2fb5edfce54cb7d8a82f700b04b
kKrishGupta <kg20060220@gmail.com>
2026-08-13 20:29:54 +0530

    initial setup dont

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-500 text-white text-4xl font-bold">
+      Hello
+    </div>
+  )
+}
+
+export default App
